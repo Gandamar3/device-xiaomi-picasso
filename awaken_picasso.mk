b082d5006258d3b256561b0d46f88d410ea9bf2c
Mango <mango@gmail.com>
2020-11-03 22:02:21 +0200

    picasso: Update Fingerprint To Coral's November Security Patch

--- awaken_picasso.mk
+++ awaken_picasso.mk
@@ -33,9 +33,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DISC="coral-user 11 RP1A.200720.009 6720564 release-keys"
+    PRIVATE_BUILD_DISC="coral-user 11 RP1A.201105.002 6869500 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.201105.002/6869500:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
