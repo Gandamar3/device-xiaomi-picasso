a4304697e0580c429de9e29277a9672cf7fc4870
Mango <mango@gmail.com>
2020-11-12 18:56:24 +0200

    picasso: enable dex2oat64

--- system_prop.mk
+++ system_prop.mk
@@ -7,3 +7,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.twsplussho=true \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
+
+# ART
+dalvik.vm.dex2oat64.enabled=true
+dalvik.vm.dex2oat-filter=speed
+dalvik.vm.image-dex2oat-filter=speed
+ro.vendor.qti.am.reschedule_service=true
+ro.sys.fw.dex2oat_thread_count=8
+dalvik.vm.boot-dex2oat-threads=8
+dalvik.vm.dex2oat-threads=8
